738d36f658d3305b0e2cf24488f8e732a196037c
Steve-Don <92700131+Steve-Don@users.noreply.github.com>
2022-07-26 18:52:21 +0930

    This version updates all the ejs files in order to make code work

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
-var userInputs = [];
-var workItems =[];
+// const userInputs = [];
+// const workItems =[];
+
 
 const app = express();
 
@@ -11,18 +14,10 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
+
 app.get("/",function(req,res){
-    var today = new Date();
-    var currentDay = today.getDay();
-
-    var options = {
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    }
-
-    var day = today.toLocaleDateString("en-US",options);
-
+    const day = date.getDate();
     res.render("list",{listTitle: day,newListItem:userInputs});
 
 });
@@ -30,7 +25,7 @@ app.get("/",function(req,res){
 app.post("/",function(req,res){
    console.log(req.body);
 
-    var item = req.body.newItem;
+   const item = req.body.newItem;
     if(req.body.list == "Work"){
         workItems.push(item);
         res.redirect("/work");
@@ -44,6 +39,9 @@ app.get("/work",function(req,res){
     res.render("list",{listTitle: "Work List", newListItem:workItems});
 });
 
+app.get("/about",function(req,res){
+    res.render("about");
+})
 
 app.listen(3000,function(){
     console.log("server started on port 3000")
---
+++ date.js
@@ -0,0 +1,15 @@
+
+exports.getDate = function(){
+
+const today = new Date();
+const currentDay = today.getDay();
+
+const options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+}
+
+return today.toLocaleDateString("en-US",options);
+}
+
